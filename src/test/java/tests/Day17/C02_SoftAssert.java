da10352bab6b29032fb9dc633cfc0691ab3bba69
fatihhhh <fatihisiteknik42@gmail.com>
2022-09-28 21:06:28 +0300

    v05

--- src/test/java/tests/Day17/C02_SoftAssert.java
+++ src/test/java/tests/Day17/C02_SoftAssert.java
@@ -1,7 +1,36 @@
 package tests.Day17;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import utilities.TestBase;
 
 public class C02_SoftAssert extends TestBase {
+         /*
+          Softassertion baslangic ve bitis satirlari arasindaki
+          tum assertion'lari yapip
+          bitis satirina geldiginde bize buldugu tum hatalari rapor eder
+          */
+
+    @Test
+    public void test01() {
+        //1-amazon anasayfaya gidin
+        driver.get("https://amazon.com");
+        //2-title in Amazon içerdigini test edin
+        Assert.assertTrue(driver.getTitle().contains("Amazon"));
+        //3-arama kutusnun erişilebilir oldugunu tets edin
+        WebElement aramakutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        Assert.assertTrue(aramakutusu.isEnabled());
+        //4-arama kutusuna Nuella yazıp aratın
+        aramakutusu.sendKeys("Nutella", Keys.ENTER);
+        //5-arama yapıldıgını test edin
+        WebElement sonuc= driver.findElement(By.xpath("//*[@class='a-section a-spacing-small a-spacing-top-small']"));
+        Assert.assertTrue(sonuc.isDisplayed());
+        //6-arama sonucunun Nutella içerdigini test edin
+        Assert.assertTrue(sonuc.getText().contains("Nutella"));
+
+    }
 
 }
---
+++ src/test/java/tests/Day17/C03_SoftAssert.java
@@ -0,0 +1,7 @@
+package tests.Day17;
+
+import utilities.TestBase;
+
+public class C03_SoftAssert extends TestBase {
+
+}
