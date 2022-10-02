365072dac557913e43804a8e1fdc109931c42214
fatihhhh <fatihisiteknik42@gmail.com>
2022-10-02 12:59:09 +0300

    v05

--- src/test/java/Pages/HmcPage.java
+++ src/test/java/Pages/HmcPage.java
@@ -10,23 +10,36 @@ public class HmcPage {
         PageFactory.initElements(Driver.getDriver(), this);
 
     }
+
     @FindBy(xpath = "//*[text()='Log in']")
     public WebElement login;
 
-    @FindBy(id="UserName")
+    @FindBy(id = "UserName")
     public WebElement userNameTexti;
 
-    @FindBy(id ="Password")
+    @FindBy(id = "Password")
     public WebElement passwordTexti;
-    @FindBy(id="btnSubmit")
+    @FindBy(id = "btnSubmit")
     public WebElement LoginButtonu;
-    @FindBy(xpath="//*[@class='username username-hide-on-mobile']")
+    @FindBy(xpath = "//*[@class='username username-hide-on-mobile']")
     public WebElement LoginKullanıcı;//
 
     @FindBy(xpath = "//*[@class='username username-hide-on-mobile']")
     public WebElement girisYapildi;
     @FindBy(id = "divMessageResult")
     public WebElement girisYapilamadiElementi;
+    @FindBy(xpath = "//*[text()='Hotel Management']")
+    public WebElement hotelmahagement;
+    @FindBy(xpath = "//a[@href='/admin/RoomReservationAdmin']")
+    public WebElement roomreservation;
 
+    @FindBy(xpath = "//*[text()='Add Room Reservation ']")
+    public WebElement addHotelButton;
+    @FindBy(id = "IDUser")
+    public WebElement İdUser;
+    @FindBy(xpath = "//*[@class='bootbox-body']")
+    public WebElement RoomReservationText;
 
+    @FindBy(xpath = "//*[text()='OK']")
+    public WebElement savabutton;
 }
--- src/test/java/tests/Day20/C03_E2ETest.java
+++ src/test/java/tests/Day20/C03_E2ETest.java
@@ -1,4 +1,61 @@
 package tests.Day20;
 
+import Pages.HmcPage;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import javax.swing.*;
+
 public class C03_E2ETest {
+    @Test
+    public void testE2E() throws InterruptedException {
+        //https://www.hotelmycamp.com adresine git.
+        Driver.getDriver().get(ConfigReader.getProperty("hmcUrl"));
+        HmcPage hmcPage = new HmcPage();
+        hmcPage.login.click();
+        //Username textbox ve password metin kutularını locate edin ve asagidaki verileri
+        //Username : manager
+        //Password  : Manager1!
+        //Login butonuna tıklayın.
+        hmcPage.userNameTexti.sendKeys(ConfigReader.getProperty("userName"));
+        hmcPage.passwordTexti.sendKeys(ConfigReader.getProperty("password"));
+        hmcPage.LoginButtonu.click();
+        //Hotel Management/Room reservation menusunden ADD ROOM RESERVATION butonuna tiklayin
+        Actions action= new Actions(Driver.getDriver());
+        hmcPage.hotelmahagement.click();
+        Thread.sleep(2000);
+        hmcPage.roomreservation.click();
+        Thread.sleep(2000);
+        hmcPage.addHotelButton.click();
+        Thread.sleep(2000);
+        //Açılan sayfadaki tüm metin kutularına istediğiniz verileri girin.
+        //Save butonuna tıklayın.
+        Faker faker = new Faker();
+        hmcPage.İdUser.sendKeys("manager");
+        action.sendKeys(Keys.TAB).sendKeys("Happy Hotel").
+                sendKeys(Keys.TAB).sendKeys("1500").sendKeys(Keys.TAB).sendKeys("10/08/2022")
+                .sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys("10/13/2022").
+                sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys("2").sendKeys(Keys.TAB).sendKeys("2")
+                .sendKeys(Keys.TAB).sendKeys(faker.name().fullName()).sendKeys(Keys.TAB).
+                sendKeys(faker.phoneNumber().cellPhone()).sendKeys(Keys.TAB)
+                .sendKeys(faker.internet().emailAddress()).sendKeys(Keys.TAB).sendKeys("skajdlaksjdaasd")
+                .sendKeys(Keys.TAB).sendKeys(Keys.SPACE).sendKeys(Keys.TAB).sendKeys(Keys.TAB)
+                .sendKeys(Keys.ENTER).perform();
+        //“RoomReservation was inserted successfully” textinin göründüğünü test edin.
+        Thread.sleep(3000);
+        Assert.assertTrue(hmcPage.RoomReservationText.isDisplayed());
+        //OK butonuna tıklayın.
+        hmcPage.savabutton.click();
+
+
+
+
+
+
+    }
 }
