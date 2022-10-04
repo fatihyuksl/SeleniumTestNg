f20f680dc248e387a09e66e2b3b92041a2a15707
fatihhhh <fatihisiteknik42@gmail.com>
2022-10-04 20:24:59 +0300

    v05

--- src/test/java/tests/day16/C03_Priority.java
+++ src/test/java/tests/day16/C03_Priority.java
@@ -26,7 +26,8 @@ public class C03_Priority extends TestBase {
 
         @Test
         public void techproedTesti() {
-            driver.get("https://www.techproeducation.com");//////git add .////dgit pushg
+            driver.get("https://www.techproeducation.com");/
+
             System.out.println(driver.getCurrentUrl());
     }
 }
