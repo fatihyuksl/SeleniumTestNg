6d5b8ca19698d87550e2e8281ff41f484bbe9bb8
fatihhhh <fatihisiteknik42@gmail.com>
2022-10-05 22:07:32 +0300

    v05

--- src/test/java/tests/day22/C01_RaporluTest.java
+++ src/test/java/tests/day22/C01_RaporluTest.java
@@ -1,7 +1,57 @@
 package tests.day22;
 
+import Pages.BrcPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
 import utilities.TestBaseRapor;
 
 public class C01_RaporluTest extends TestBaseRapor {
 
+          /*
+           -https://www.bluerentalcars.com/ adresine git
+           -login butonuna bas
+           -test data user email: customer@bluerentalcars.com ,
+           -test data password : 12345 dataları girip login e basın
+           -login butonuna tiklayin
+           -Degerleri girildiginde sayfaya basarili sekilde girilebildigini test et
+            */
+
+    @Test
+    public void test01() {
+        extentTest = extentReports.createTest("Positive Login", "Valid username");
+        // Bir test method olustur positiveLoginTest()
+        //  https://www.bluerentalcars.com/ adresine git
+        Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
+        extentTest.info("Brc anasayfaya gidildi");
+        BrcPage brcPage = new BrcPage();
+        //-https://www.bluerentalcars.com/ adresine git
+
+        //      login butonuna bas
+        brcPage.ilkLoginButonu.click();
+        extentTest.info("login butonuna tiklandi");
+
+        //test data user email: customer@bluerentalcars.com ,
+        brcPage.emailTextBox.sendKeys(ConfigReader.getProperty("brcValidEmail"));
+        extentTest.info("Gecerli email yazdirildi");
+
+        //test data password : 12345
+        brcPage.passwordTextBox.sendKeys(ConfigReader.getProperty("brcValidPassword"));
+        extentTest.info("Gecerli sifre yazildi");
+
+        //login butonuna tiklayin
+        brcPage.ikinciLoginButonu.click();
+        extentTest.info("Login butonuna tiklandi");
+
+        //Degerleri girildiginde sayfaya basarili sekilde girilebildigini test et
+        Assert.assertTrue(brcPage.kullaniciProfilIsmi.isDisplayed());
+        extentTest.info("Kullanici basarili bir sekilde giris yapti");
+
+        Driver.closeDriver();
+    }
+
+
 }
+
+
