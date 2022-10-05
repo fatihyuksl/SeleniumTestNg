2ea7bb3c57da9517cf9a8ee5d9401974de281961
fatihhhh <fatihisiteknik42@gmail.com>
2022-10-05 20:42:29 +0300

    v05

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -132,7 +132,7 @@ public class ReusableMethods {
         }
     }
 
-    static Faker faker;/////git add .4//ssee//45
+    static Faker faker;
     static Actions actions;
     static SoftAssert softAssert;
     static Select select;
