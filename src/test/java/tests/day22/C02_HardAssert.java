e255f7dd7fef85414fea09244f8de446eb2f5f5f
fatihhhh <fatihisiteknik42@gmail.com>
2022-10-20 23:33:24 +0300

    v06

--- src/test/java/tests/day22/C02_HardAssert.java
+++ src/test/java/tests/day22/C02_HardAssert.java
@@ -29,7 +29,7 @@ public class C02_HardAssert extends TestBaseCross {
         //5-arama yapıldıgını test edin
         WebElement sonuc= driver.findElement(By.xpath("//*[@class='a-section a-spacing-small a-spacing-top-small']"));
         Assert.assertTrue(sonuc.isDisplayed());
-        //6-arama sonucunun Nutella içerdigini test edin2
+        //6-arama sonucunun Nutella içerdigini test edin23
         Assert.assertTrue(sonuc.getText().contains("Nutella"));
 
     }
