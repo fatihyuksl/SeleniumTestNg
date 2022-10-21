ef0636deff73b009a37924cd8076b32d63c119b4
fatihhhh <fatihisiteknik42@gmail.com>
2022-10-21 22:20:20 +0300

    v06

--- src/test/java/tests/day21/C01_ScreenShotReusableMethod.java
+++ src/test/java/tests/day21/C01_ScreenShotReusableMethod.java
@@ -27,7 +27,7 @@ public class C01_ScreenShotReusableMethod {
         //nutella aratınız
         AmazonPage amznpage=new AmazonPage();
         amznpage.aramakutusu.sendKeys("Nutella", Keys.ENTER);
-        // arama sonuc yazısı webelementinin resmini alınız12345678
+        // arama sonuc yazısı webelementinin resmini alınız123456789
         ReusableMethods.getScreenshotWebElement("AramaSonucWe",amznpage.aramaSonucuElementi);
     }
 }
