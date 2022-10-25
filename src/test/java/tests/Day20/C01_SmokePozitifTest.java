41cf28a02e527f4fe3a4840a0e131913b82ae1ba
fatihhhh <fatihisiteknik42@gmail.com>
2022-10-25 22:22:59 +0300

    v06

--- src/test/java/tests/Day20/C01_SmokePozitifTest.java
+++ src/test/java/tests/Day20/C01_SmokePozitifTest.java
@@ -21,7 +21,7 @@ public class C01_SmokePozitifTest {
         page classımızdaki locate'lerimize ulaşabiliriz
          */
             hmcPage.login.click();
-            //test data username: manager ,
+            //test data username: manager ,1
             //test data password : Manager1!
             hmcPage.userNameTexti.sendKeys(ConfigReader.getProperty("user"));
             Actions actions = new Actions(Driver.getDriver());
