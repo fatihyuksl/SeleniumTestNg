f9ef41ec8a55a60f0607403b54867532b3fd091c
fatihhhh <fatihisiteknik42@gmail.com>
2022-10-26 21:47:41 +0300

    v05

--- src/test/java/tests/Day20/C01_SmokePozitifTest.java
+++ src/test/java/tests/Day20/C01_SmokePozitifTest.java
@@ -22,7 +22,7 @@ public class C01_SmokePozitifTest {
          */
             hmcPage.login.click();
             //test data username: manager ,
-            //test data password : Manager1!.12345678
+            //test data password : Manager1!.123456789
             hmcPage.userNameTexti.sendKeys(ConfigReader.getProperty("user"));
             Actions actions = new Actions(Driver.getDriver());
             actions.sendKeys(Keys.TAB).sendKeys(ConfigReader.getProperty("password"))
